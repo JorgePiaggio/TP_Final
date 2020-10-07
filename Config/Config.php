345e474fd7be1bbb52f11eeeb8f100077cc2047f
maxi2991 <commissomaximiliano@gmail.com>
2020-10-07 11:56:49 -0300

    Mod root

--- Config/Config.php
+++ Config/Config.php
@@ -2,11 +2,11 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
-define("FRONT_ROOT", "/Practica-Enunciados/TP6/ExerciseOne/");
+define("FRONT_ROOT", ROOT."TP_Final/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "layout/styles/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
-define("IMG_PATH", VIEWS_PATH . "img/");
+define("IMG_PATH", FRONT_ROOT.VIEWS_PATH . "img/");
 ?>
 
 
