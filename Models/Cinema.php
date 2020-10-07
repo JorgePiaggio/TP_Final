6facd08521187caa4fdc9db121d0fe796c8abe47
Francisco Grande <franciscogrande91@gmail.com>
2020-10-07 15:02:22 -0300

    Creacion de CinemaDAO con json

--- Models/Cinema.php
+++ Models/Cinema.php
@@ -3,6 +3,7 @@ namespace Models;
 
 class Cinema{
 
+    private $id;
     private $name;
     private $address;
     private $phone;
@@ -10,10 +11,12 @@ class Cinema{
 
     function __construct(){}
     
+    function getId(){return $this->id;}
     function getName(){return $this->name;}
     function getAddress(){return $this->address;}
     function getPhone(){return $this->phone;}
     function getEmail(){return $this->email;}
+    function setId($id){$this->id=$id;}
     function setName($name){$this->name=$name;}
     function setAddress($address){$this->address=$address;}
     function setPhone($phone){$this->phone=$phone;}
