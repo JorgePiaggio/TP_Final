441515274311d178c66e204b13da42f5daa3e015
maxi2991 <commissomaximiliano@gmail.com>
2020-10-07 12:44:06 -0300

    mod Config2

--- Config/Config.php
+++ Config/Config.php
@@ -2,11 +2,13 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
-define("FRONT_ROOT", ROOT."TP_Final/");
+define("FRONT_ROOT", "TP_Final/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "layout/styles/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
-define("IMG_PATH", FRONT_ROOT.VIEWS_PATH . "img/");
+define("IMG_PATH", VIEWS_PATH . "img/");
+define("MODELS_PATH","Models/");
+define("DAO_PATH","DAO/");
 ?>
 
 
