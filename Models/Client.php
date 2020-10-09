a45e89165e677da12187dc3650e17f107e4493c4
maxi2991 <commissomaximiliano@gmail.com>
2020-10-09 14:13:37 -0300

    set and get CardList

--- Models/Client.php
+++ Models/Client.php
@@ -38,6 +38,8 @@ class Client{
     public function getPass(){return $this->password;}
     public function setBillList($billList){$this->billList=$billList;}
     public function getBillList(){return $this->billList;}
+    public function setCardList($cardList){$this->cardList=$cardList;}
+    public function getCardList(){return $this->cardList;}
     public function addBill($bill){array_push($this->billList,$bill);}
     public function addCard($card){array_push($this->cardList,$card);}
     
