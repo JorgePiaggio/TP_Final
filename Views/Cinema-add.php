a44f4899847e90d3f499d1b52a66319aea871a95
JorgePiaggio <proto2345@protonmail.com>
2020-10-11 13:23:05 -0300

    floating-labels

--- Views/Cinema-add.php
+++ Views/Cinema-add.php
@@ -2,41 +2,38 @@
      <section id="listado" class="mb-5">
           <form action="<?php echo FRONT_ROOT?>Cinema/Add" method="post">
           <div class="container">
-               <h3 class="mb-3">Add Cinema</h3>
+               <h3 class="form-title">Add Cinema</h3>
 
-               <div class="bg-light-p-4">
-                    <div class="row">
-                         <div class="col-lg-3">
-                              <label for="">Cinema</label>
-                              <input type="text" name="name" class="form-control form-control-ml" required>
+                    <div class="floating-label-form">
+                         <div class="floating-label">
+                              <input type="text" name="name" placeholder=" " class="floating-input" required>
+                              <span class="highlight"></span><label for="">Cinema</label>
                          </div>                         
-                         
-                         <div class="col-lg-3">
-                              <label for="">Street</label>
-                              <input type="text" name="street" class="form-control form-control-ml" required>
+
+                         <div class="floating-label">
+                              <input type="text" name="street" placeholder=" " class="floating-input" required>
+                              <span class="highlight"></span><label for="">Street</label>
                          </div>
 
-                         <div class="col-lg-3">
-                              <label for="">Number</label>
-                              <input type="number" name="number" class="form-control form-control-ml" required>
+                         <div class="floating-label">
+                              <input type="number" name="number" placeholder=" " class="floating-input" required>
+                              <span class="highlight"></span><label for="">Number</label>
                          </div>
 
-                         <div class="col-lg-3">
-                              <label for="">Phone</label>
-                              <input type="number" name="phone" class="form-control form-control-ml" required>
+                         <div class="floating-label">
+                              <input type="number" name="phone" placeholder=" " class="floating-input" required>
+                              <span class="highlight"></span><label for="">Phone</label>
                          </div>
 
-                         <div class="col-lg-3">
-                              <label for="">Email</label>
-                              <input type="email" name="email" class="form-control form-control-ml" required>
+                         <div class="floating-label">
+                              <input type="email" name="email" placeholder=" " class="floating-input" required>
+                              <span class="highlight"></span><label for="">Email</label>
                          </div>
-                         <div class="col-lg-3">
+                         <div class="floating-label">
                               <span>&nbsp;</span>
                               <button type="submit" name="" class="btn btn-primary ml-auto d-block">Agregar</button>
                          </div>
-
                     </div>                    
-               </div>
           </div>
           </form>
      </section>
