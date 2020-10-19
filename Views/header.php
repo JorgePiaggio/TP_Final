ceb6b31706a817bc18880a651a22262f330af232
Francisco Grande <franciscogrande91@gmail.com>
2020-10-19 02:09:57 -0300

    logoHeader and changeFontTitle

--- Views/header.php
+++ Views/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <title>MoviePass</title>
+<link rel="icon" type="image/png" href="<?php echo IMG_PATH?>/icons/logo.png">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link href="<?php echo CSS_PATH;?>layout.css" rel="stylesheet" type="text/css" media="all">
