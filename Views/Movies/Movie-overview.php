125516097e07a8642b0bf58bfaab5da2fbc47660
JorgePiaggio <proto2345@protonmail.com>
2020-10-22 12:55:28 -0300

    movieOverview fixes

--- Views/Movies/Movie-overview.php
+++ Views/Movies/Movie-overview.php
@@ -34,15 +34,14 @@
                                                           $str .=" ".$director." -";}
                                                             echo substr_replace($str,"", -1); ?></p>
             </div>
-            <div>
-              <p><h4>Description</h4><?php echo $movie->getDescription()?></p>
-            </div>
-            <div>
-              <p><h4>Trailer</h4><?php if(!$movie->getVideoPath()){
-                                        echo "Not Available";}else{ ?></p>
-                                          <iframe width="440" height="300" src="https://www.youtube.com/embed/<?php echo $movie->getVideoPath();?>" 
-                                          frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                                          </iframe> <?php } ?>
+            
+            <div>                                           <!-- si hay trailer se muestra, si no en ese espacio se muestra la descripcion -->
+              <?php if(!$movie->getVideoPath()){ ?>     
+                      <p><h4>Description</h4><?php echo $movie->getDescription()?></p>
+                      <?php }else{ ?></p><p><h4>Trailer</h4>
+                      <iframe width="440" height="300" src="https://www.youtube.com/embed/<?php echo $movie->getVideoPath();?>" 
+                      frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                      </iframe> <?php } ?>
             </div>
           </div>   
               <!-- ####################################### COLUMNA DERECHA - POSTER ######################################################### -->
@@ -51,6 +50,10 @@
           </div>
         </div>
         <div class="container clear up cardStyle3">
+            <div>
+              <?php if($movie->getVideoPath()){ ?>           <!-- si hay trailer, la descripcion se muestra debajo -->
+              <p><h4>Description</h4><?php echo $movie->getDescription()?></p>
+              </div> <?php } ?>
             <p><h4>Review</h4><?php if($movie->getReview()){
                                     echo $movie->getReview()['content'];} else {echo "Not reviewed yet";} ?></p>
             <p class= "fl_right"><?php if($movie->getReview()){
