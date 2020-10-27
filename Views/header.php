4554db35653509e014209d60d9be685d7a7cb0b2
JorgePiaggio <proto2345@protonmail.com>
2020-10-26 21:13:43 -0300

    checkbox

--- Views/header.php
+++ Views/header.php
@@ -5,7 +5,7 @@
 <link rel="icon" type="image/png" href="<?php echo IMG_PATH?>/icons/logo.png">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link href="<?php echo CSS_PATH;?>layout.css" rel="stylesheet" type="text/css" media="all">
+<link href="<?php echo CSS_PATH;?>layout.scss" rel="stylesheet" type="text/css" media="all">
 <link rel="stylesheet" href="<?php echo CSS_PATH;?>fontawesome-4.6.3.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Faster+One&display=swap" rel="stylesheet">
     <style>   
