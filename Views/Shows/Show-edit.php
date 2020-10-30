8ac56d942cd3e1c5ffbee35e9fd0e9caf6cb126b
maxi2991 <commissomaximiliano@gmail.com>
2020-10-30 14:06:01 -0300

    details in form edit show

--- Views/Shows/Show-edit.php
+++ Views/Shows/Show-edit.php
@@ -13,7 +13,9 @@
                                               <?php if(!$roomList){?>
                                                 <option value="">No Rooms Found</option>
                                               <?php }?>
+                                              <option value="<?php echo $editShow->getRoom()->getIdRoom(); ?>" selected disabled><?php echo $editShow->getRoom()->getName(); ?></option>
                                             <?php foreach($roomList as $room){ ?>
+                                                
                                             <option value="<?php echo $room->getIdRoom(); ?>"><?php echo $room->getName(); ?></option>
                                             <?php }?>
                                         </select>                    
@@ -24,7 +26,9 @@
                                         <?php if(!$movieList){?>
                                                 <option value="">No actives movies Found</option>
                                               <?php }?>
+                                              <option value="<?php echo $editShow->getMovie()->getTmdbId(); ?>" selected disabled><?php if(strlen($editShow->getMovie()->getTitle())>30){$title=substr($editShow->getMovie()->getTitle(),0,30); echo $title."...";}else{echo $editShow->getMovie()->getTitle();} ?></option>
                                             <?php foreach($movieList as $movie){ ?>
+                                                
                                                 <option value="<?php echo $movie->getTmdbId(); ?>"><?php if(strlen($movie->getTitle())>30){$title=substr($movie->getTitle(),0,30); echo $title."...";}else{echo $movie->getTitle();} ?></option>
                                             <?php }?>
                                         </select>                    
